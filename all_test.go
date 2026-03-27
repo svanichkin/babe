2ec5757fea813fefe2d7de50a150137bb42ee0ab
s.vanichkin <github@macflash.ru>
2026-03-27 18:53:32 +0300

    Update all_test.go

--- all_test.go
+++ all_test.go
@@ -586,6 +586,34 @@ func BenchmarkBABEDecodeCmd4JPGFilmgrain2Blur8(b *testing.B) {
 	}
 }
 
+func BenchmarkBABEDecodeCmd4JPG(b *testing.B) {
+	img := loadTestImagePrefer(b, "4.jpg", "benchmark.jpg")
+	levels, err := blocksFromSpec("8-128")
+	if err != nil {
+		b.Fatalf("blocksFromSpec: %v", err)
+	}
+	enc := NewEncoder()
+	enc.patternCount = 64
+	enc.backgroundTile = 32
+	enc.yQuantShift = 4
+	enc.levels = levels
+	comp, err := enc.Encode(img, 0, false)
+	if err != nil {
+		b.Fatalf("Encode: %v", err)
+	}
+	dec := NewDecoder()
+	if _, err := dec.Decode(comp); err != nil {
+		b.Fatalf("warmup decode: %v", err)
+	}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		if _, err := dec.Decode(comp); err != nil {
+			b.Fatalf("Decode: %v", err)
+		}
+	}
+}
+
 func BenchmarkBABEDecodeQ0Patterns64Blocks16_128Tile32Q4Benchmark(b *testing.B) {
 	img := loadTestImagePrefer(b, "benchmark.jpg")
 	levels, err := blocksFromSpec("16-128")
